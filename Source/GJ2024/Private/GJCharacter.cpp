0bb25fc1e682688476dd245694a012c8f5231cf0
ttofff <255374409@qq.com>
2024-11-18 19:40:00 +0800

    添加了角色的基本操作

--- Source/GJ2024/Private/GJCharacter.cpp
+++ Source/GJ2024/Private/GJCharacter.cpp
@@ -3,12 +3,29 @@
 
 #include "GJCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 // Sets default values
-AGJCharacter::AGJCharacter()
+AGJCharacter::AGJCharacter():
+RotationRate(0.3f)
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;//移动朝向运动
+
+	PlayerCameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
+	PlayerCameraSpringArm->SetupAttachment(RootComponent);
+	PlayerCameraSpringArm->bUsePawnControlRotation = true;
+	
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	PlayerCamera->SetupAttachment(PlayerCameraSpringArm);
+	PlayerCamera->bUsePawnControlRotation = false;
+	
 }
 
 // Called when the game starts or when spawned
@@ -30,5 +47,80 @@ void AGJCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	check(PlayerInputComponent);
+	
+	//基本操作
+	PlayerInputComponent->BindAxis("Look", this, &AGJCharacter::Look);
+	PlayerInputComponent->BindAxis("Turn", this, &AGJCharacter::Turn);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGJCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGJCharacter::MoveRight);
+	PlayerInputComponent->BindAction("Accelerated", IE_Pressed, this, &AGJCharacter::Accelerate);
+	PlayerInputComponent->BindAction("Accelerated", IE_Released, this, &AGJCharacter::Decelerate);
+	
+}
+
+//上下移动视角
+void AGJCharacter::Look(float value)
+{
+	float NewValue = ClampCameraRotation + value * RotationRate;
+	if (NewValue <= 40.f && NewValue >= -10.f)
+	{
+		ClampCameraRotation = NewValue;
+		AddControllerPitchInput(value * RotationRate);
+	}
+	else
+	{
+		if (NewValue > 40.f)
+		{
+			ClampCameraRotation = 40.f;
+		}
+		else ClampCameraRotation = -10.f;
+	}
+	// GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red,
+	// 			FString::Printf(TEXT("ClampCameraRotation：%f"),ClampCameraRotation));
+}
+
+//左右移动视角
+void AGJCharacter::Turn(float value)
+{
+	AddControllerYawInput(value * RotationRate);
+}
+
+//前后移动
+void AGJCharacter::MoveForward(float value)
+{
+	if ((Controller == nullptr) || (value != 0.0f))
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, value);
+	}
+}
+
+//左右移动
+void AGJCharacter::MoveRight(float value)
+{
+	if ((Controller == nullptr) || (value != 0.0f))
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, value);
+	}
+}
+
+//加速
+void AGJCharacter::Accelerate()
+{
+	GetCharacterMovement()->MaxWalkSpeed = AcceleratedSpeed;
+}
+
+//减速
+void AGJCharacter::Decelerate()
+{
+	GetCharacterMovement()->MaxWalkSpeed = OriginalSpeed;
 }
 
--- Source/GJ2024/Public/GJCharacter.h
+++ Source/GJ2024/Public/GJCharacter.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "GJCharacter.generated.h"
 
 UCLASS()
@@ -11,13 +13,43 @@ class GJ2024_API AGJCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* PlayerCameraSpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* PlayerCamera;
+
 public:
-	// Sets default values for this character's properties
-	AGJCharacter();
+	//限制相机旋转角度
+	float ClampCameraRotation = 0.f;
+
+	//限制相机旋转速度
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Camera")
+	float RotationRate;
+
+	//原始速度
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Speed")
+	float OriginalSpeed;
+
+	//疾跑速度
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Speed")
+	float AcceleratedSpeed;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void Look(float value);
+
+	void Turn(float value);
+
+	void MoveForward(float value);
+
+	void MoveRight(float value);
+
+	void Accelerate();
+
+	void Decelerate();
 
 public:	
 	// Called every frame
@@ -25,5 +57,8 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
+	// Sets default values for this character's properties
+	AGJCharacter();
+	
 };
